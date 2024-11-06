9289e07f4e900e3a0914037030ca8901c26b187b
zhangshunsheng <1065355934@qq.com>
2024-11-06 18:41:35 +0800

    six commit

--- main.py
+++ main.py
@@ -27,8 +27,12 @@ def print_hi5(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+def print_hi6(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 # Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
 
